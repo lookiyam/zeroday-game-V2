adfdc87020ae96f922967622d65d8787933f8354
kelseyleefalzon <kelsey.falzon.d56573@mcast.edu.mt>
2021-01-14 14:59:44 +0100

    Commit 1
    Start Menu

---
+++ Assets/Scripts/MainMenu/MainMenuControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour {
+
+public void selectScene(){
+		switch (this.gameObject.name) {
+		case "Tutorial":
+			SceneManager.LoadScene ("Tutorial");
+			break;
+		case "Deploy":
+			SceneManager.LoadScene ("Level1");
+			break;
+		case "Abandon":
+			SceneManager.LoadScene ("Scene03");
+			break;
+	}
+}
+
+}
