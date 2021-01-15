7abd443c0eafc00e0378f83af57f6338eebfe913
lookiyam <56125496+lookiyam@users.noreply.github.com>
2021-01-15 18:05:57 +0100

    start menu tutorial menu
    start button, tutorial button, quit button, tutorial screen, tutorial quit button IMPLEMENTED

---
+++ Assets/Scripts/MainMenu/BackButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackButtonScript : MonoBehaviour {
+
+	
+	public void backToMainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
---
+++ Assets/Scripts/MainMenu/MainMenuControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour {
+
+public void selectScene(){
+		switch (this.gameObject.name) {
+		case "Tutorial":
+			SceneManager.LoadScene ("Tutorial");
+			break;
+		case "Start":
+			SceneManager.LoadScene ("Level1");
+			break;
+		case "Quit":
+			SceneManager.LoadScene ("Scene03");
+			break;
+	}
+}
+
+}
