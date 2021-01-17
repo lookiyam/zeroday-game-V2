f3d2f37a8eb8e7ecc53fa22595a8c5088fbeb994
lookiyam <56125496+lookiyam@users.noreply.github.com>
2021-01-17 15:18:35 +0100

    Implamented Sprite Change script
    Tutorial Sprite change system Implementation WIP

---
+++ Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ChangeSprite : MonoBehaviour {
+
+	private SpriteRenderer rend;
+	private Sprite catSprite, monsterSprite;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer>();
+		catSprite = Resources.Load<Sprite>("Cat");
+		monsterSprite = Resources.Load<Sprite>("Monster");
+		rend.sprite = catSprite;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			if (rend.sprite == catSprite)
+				rend.sprite = monsterSprite;
+					else if (rend.sprite == monsterSprite)
+						rend.sprite = catSprite;
+		}
+	}
+}
